4acd64d6736ebe8defb3725ad9006b1640469930
Ian Bittencourt Andrade <ianbandrade14@gmail.com>
2021-01-29 21:13:07 -0300

    update card structure

--- src/components/card.js
+++ src/components/card.js
@@ -1,44 +1,40 @@
 import React from "react"
 import { Link } from "gatsby"
 import Img from "gatsby-image"
+import { Box, Badge, Heading, Flex, Text, Divider } from "@chakra-ui/react"
 
 const Card = ({ article }) => {
   return (
     <Link to={`/article/${article.node.slug}`} className="uk-link-reset">
-      <div className="uk-card uk-card-muted">
-        <div className="uk-card-media-top">
+      <Box p={2}>
+        <Box>
           <Img
             fixed={article.node.image.childImageSharp.fixed}
             imgStyle={{ position: "static" }}
           />
-        </div>
-        <div className="uk-card-body">
-          <p id="category" className="uk-text-uppercase">
-            {article.node.category.name}
-          </p>
-          <p id="title" className="uk-text-large">
-            {article.node.title}
-          </p>
-          <div>
-            <hr className="uk-divider-small" />
-            <div className="uk-grid-small uk-flex-left" data-uk-grid="true">
-              <div>
+        </Box>
+        <Box>
+          <Badge variant="outline">{article.node.category.name}</Badge>
+          <Heading mb={1}>{article.node.title}</Heading>
+          <Box>
+            <Divider orientation="horizontal" />
+            <Flex>
+              <Box w="40px">
                 {article.node.author.picture && (
                   <Img
                     fixed={article.node.author.picture.childImageSharp.fixed}
                     imgStyle={{ position: "static", borderRadius: "50%" }}
+                    alt="Avatar autor"
                   />
                 )}
-              </div>
-              <div className="uk-width-expand">
-                <p className="uk-margin-remove-bottom">
-                  {article.node.author.name}
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+              </Box>
+              <Box w="200px">
+                <Text fontSize="lg">{article.node.author.name}</Text>
+              </Box>
+            </Flex>
+          </Box>
+        </Box>
+      </Box>
     </Link>
   )
 }
